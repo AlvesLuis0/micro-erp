ab8f74d859855ca36dfeb7068992919dad7bb012
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-03-13 18:12:42 -0300

    feat: add validation to person details and contacts

---
+++ app/models/contact.rb
@@ -0,0 +1,25 @@
+class Contact < ApplicationRecord
+  include ContactType::Enum
+
+  belongs_to :person_detail
+
+  validates :email, length: { maximum: 100 }, if: :contact_type_email?
+
+  with_options if: :contact_type_mobile? do |mobile|
+    mobile.validates :area_code, numericality: { only_integer: true }, length: { is: 2 }
+    mobile.validates :mobile_number, numericality: { only_integer: true }, length: { is: 9 }
+  end
+
+  before_validation :clear_attributes
+
+  protected
+
+  def clear_attributes
+    attributes_to_clear = if contact_type_email?
+      [ :area_code, :mobile_number ]
+    elsif contact_type_mobile?
+      [ :email ]
+    end
+    super(attributes_to_clear)
+  end
+end
--- app/models/person_detail.rb
+++ app/models/person_detail.rb
@@ -1,2 +1,5 @@
 class PersonDetail < ApplicationRecord
+  has_many :contacts
+
+  validates :name, presence: true, length: { maximum: 100 }
 end
