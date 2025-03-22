1d690b908438577f5e437594a50726f3b34f6409
AlvesLuis0 <alves.luis.3002@gmail.com>
2025-03-22 12:34:20 -0300

    refactor: remove unnecessary validation

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,10 +1,3 @@
 class State < ApplicationRecord
   has_many :cities
-
-  validates :description, presence: true, length: { maximum: 60 }
-  validates :uf, presence: true, length: { is: 2 }
-
-  def to_s
-    description
-  end
 end
